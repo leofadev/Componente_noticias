09dac3ef29ef723173fe19a4d76a715390626329
leonardo <pintoleonardo1023@gmail.com>
2023-09-16 20:22:53 -0500

    new variables

--- js/index.js
+++ js/index.js
@@ -1,10 +1,37 @@
-const reaction = document.querySelector(".reaction_meGusta");
-const button = document.querySelector(".me_gusta");
+// const reaction = document.querySelector(".reaction_meGusta");
+// const button = document.querySelector(".me_gusta");
 
-let contador = 0;
+// let contador = 0;
 
-button.addEventListener("click", () => {
-    console.log("Click");
-    contador = 1;
-    reaction.textContent = contador;
+// button.addEventListener("click", () => {
+// console.log("Click");
+// contador = 1;
+// reaction.textContent = contador;
+// });
+
+// Obtener una referencia al botón y al contador
+const botonMeGusta = document.getElementById('botonMeGusta');
+const contador = document.getElementById('contador_MeGusta');
+
+// Inicializar el estado del botón (sin "Me gusta" inicialmente)
+let liked = false;
+let likes = 0;
+
+// Mostrar la cantidad inicial de "Me gusta"
+contador.textContent = likes;
+
+// Agregar un evento de clic al botón
+botonMeGusta.addEventListener('click', function () {
+if (liked) {
+likes--; // Decrementar el contador si ya le dio "Me gusta"
+} else {
+likes++; // Incrementar el contador si no había dado "Me gusta"
+}
+
+// Actualizar el estado del botón y el contador
+liked = !liked;
+contador.textContent = likes;
+
+// Actualizar el texto del botón según el estado actual
+botonMeGusta.textContent = liked ? "Quitar Me gusta" : "Me gusta";
 });
